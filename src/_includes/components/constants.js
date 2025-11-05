649aa1438536caed0e6808474ae0ed831550a8c0
AdithyaPadmashali <adithya.p2001@gmail.com>
2025-11-05 12:37:19 +0530

    Change resume link

--- src/_includes/components/constants.js
+++ src/_includes/components/constants.js
@@ -1,4 +1,4 @@
 // src/_includes/components/constants.js
 // Central place for shared constants
 
-export const RESUME_LINK = "https://drive.google.com/file/d/1q-XylrWNLm3vWXInTXFUKCSasfzmrfNk/view?usp=sharing";
+export const RESUME_LINK = "https://drive.google.com/file/d/1JmiV055bNz0k1HVQwu4d6aTyYXH36iv1/view?usp=sharing";
